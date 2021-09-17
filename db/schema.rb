91abf42a5add5c049ff2687ff2ef94f36c689411
ryotagonG <ryotapb715@gmail.com>
2021-09-17 12:17:03 +0000

    customersコントローラーを編集しました

--- db/schema.rb
+++ db/schema.rb
@@ -47,14 +47,14 @@ ActiveRecord::Schema.define(version: 2021_09_17_084513) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "front_name", null: false
-    t.string "back_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "post_code", null: false
-    t.string "address", null: false
-    t.string "tel_number", null: false
-    t.boolean "is_customer_status", default: false, null: false
+    t.string "front_name"
+    t.string "back_name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "post_code"
+    t.string "address"
+    t.string "tel_number"
+    t.boolean "is_customer_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
