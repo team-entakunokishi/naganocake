67951fe31977aa795ca741e8753d301c69fa6cac
hiroyukiuchiyama <uchiyamaworld@gmail.com>
2021-09-18 11:56:34 +0000

    item edit開始

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,2 +1,39 @@
 class Admins::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+    @genres = Genre
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render action:new
+    end
+  end
+
+  def show
+   @item = Item.find(params[:id])
+  end
+
+  def edit
+   @item = Item.find(params[:id])
+   @genresv = Genre.all
+  end
+
+  def update
+   @item = Item.find(Params[:id])
+   @item.update(item_parms)
+  end
+
+  private
+
+  def Item_params
+    params.require(:item).permit(:aaaaa)
+  end
 end
