1a8554b7ac4199c2ab75acef7de044357037f017
ryotagonG <ryotapb715@gmail.com>
2021-09-18 11:39:47 +0000

    items_ordersコントローラーを記述

--- app/controllers/admins/items_orders_controller.rb
+++ app/controllers/admins/items_orders_controller.rb
@@ -1,2 +1,9 @@
 class Admins::ItemsOrdersController < ApplicationController
+  before_action :authenticate_admin!
+  def show
+    @order = Order.find(params[:id])
+  end
+  
+  def update
+  end
 end
