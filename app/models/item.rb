ad7881dfc9778b59bd6aceb2cf95f68a0ef851cd
ryotagonG <ryotapb715@gmail.com>
2021-09-20 10:34:21 +0000

    アソシエーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :type
 	has_many :cart_items, dependent: :destroy
+	has_many :item_orders, dependent: :destroy
 	validates :name, presence: true
   validates :body, presence: true
   validates :price, presence: true
