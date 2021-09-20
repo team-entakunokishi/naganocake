1e662dd588f1f8632420825e9b892ee06f19eb6a
04-04-hata <zhenlinaitian@gmail.com>
2021-09-20 09:09:36 +0000

    costomersのnamespace作成と、admin側のordersコントローラーのresourcesにindexを追加

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,24 @@
+class Public::CustomersController < ApplicationController
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def confirm
+    
+  end
+  
+  def withdraw
+    
+  end
+
+  
+end
