dc62d89a614d3fba19074c97ec0c18dec5b0800e
ryotagonG <ryotapb715@gmail.com>
2021-09-21 10:34:52 +0000

    ボタンの追加

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,18 +1,18 @@
 class Admins::CustomersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @customers = Customer.all
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
@@ -21,9 +21,9 @@ class Admins::CustomersController < ApplicationController
       render  :edit
     end
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:front_name, :back_name, :first_name, :last_name, :email, :post_code, :address, :tel_number, :is_customer_status)
   end
