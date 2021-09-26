f85ae6b9e860b9875ffd1b5e2684042792c1a9f9
ryotagonG <ryotapb715@gmail.com>
2021-09-26 07:47:27 +0000

    テスト用

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,8 @@ class Address < ApplicationRecord
   validates :shipping_code, presence: true
   validates :shipping_address, presence: true
   validates :post_name, presence: true
+  
+  def view_address_all
+    self.shipping_code + self.shipping_address + self.post_name
+  end
 end
